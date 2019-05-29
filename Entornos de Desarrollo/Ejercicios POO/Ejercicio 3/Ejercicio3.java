a04a71cb6cf8219b58ff2b60556e8687e0fbaf5f
Juanan <43585643+jonrula@users.noreply.github.com>
2019-05-29 12:22:01 +0200

    Add files via upload

---
+++ Entornos de Desarrollo/Ejercicios POO/Ejercicio 3/Ejercicio3.java
@@ -0,0 +1,19 @@
+public class Ejercicio3 {
+
+
+    public static void main(String[] args) {
+
+//Creamos los objetos
+        Comercial c1 = new Comercial(300, "DDR", 37, 1000);
+        Repartidor r1 = new Repartidor("zona 3", "Fer", 26, 900);
+
+//Llamamos a plus
+        c1.plus();
+        r1.plus();
+
+//Mostramos la informacion
+        System.out.println(c1);
+        System.out.println(r1);
+    }
+
+}
